7c0eca7210e5bdba91a13aaa72cc5c83445765eb
Petrenko Anton <antptrnk@gmail.com>
2019-06-03 19:00:02 +0300

    Remove DER/PEM conversion

--- index.php
+++ index.php
@@ -7,45 +7,30 @@ use Virgil\CryptoImpl\VirgilCrypto;
 $inputPassword = "qwerty123456";
 $privateKeyPassword = 'passw0rd'; // Can be blank
 
-// Using Virgil SDK, generate a Recovery Keypair
+// Generate keypair:
 $virgilCrypto = new VirgilCrypto();
 $keyPair = $virgilCrypto->generateKeys();
 
 $privateKey = $keyPair->getPrivateKey();
 $publicKey = $keyPair->getPublicKey();
 
-// Store the Public Key in your database and save the Private Key securely on another external device. Use PEM-format!
-// Note: you won’t be able to recover your Private Key so it is crucial not to lose it
+// Store exported keys:
+$privateKeyExported = $virgilCrypto->exportPrivateKey($privateKey, $privateKeyPassword);
+$publicKeyExported = $virgilCrypto->exportPublicKey($publicKey);
 
-// Your database should contain columns for uid, pure records and user passwords hashes, encrypted with the newly generated public key.
-// The last column will be necessary for the recovery process
+// Import keys:
+$privateKeyImported = $virgilCrypto->importPrivateKey($privateKeyExported, $privateKeyPassword);
+$publicKeyImported = $virgilCrypto->importPublicKey($publicKeyExported);
 
-$privateKeyExportDER = $virgilCrypto->exportPrivateKey($privateKey, $privateKeyPassword);
-$publicKeyExportDER = $virgilCrypto->exportPublicKey($publicKey);
-
-$privateKeyPEM = \VirgilKeyPair::privateKeyToPEM($privateKeyExportDER, $privateKeyPassword);
-$publicKeyPEM = \VirgilKeyPair::publicKeyToPEM($publicKeyExportDER);
-
-// Encrypt user password hashes with the generated Recovery Key and save them to the column mentioned in the
-// previous step
-
-$privateKeyDER = \VirgilKeyPair::privateKeyToDER($privateKeyPEM, $privateKeyPassword);
-$privateKeyImportDER = $virgilCrypto->importPrivateKey($privateKeyDER, $privateKeyPassword);
-
-$publicKeyDER = \VirgilKeyPair::publicKeyToDER($publicKeyPEM);
-$publicKeyImportDER = $virgilCrypto->importPublicKey($publicKeyDER);
-
-$encrypted = $virgilCrypto->encrypt($inputPassword, [$publicKeyImportDER]);
+// Encrypt:
+$encrypted = $virgilCrypto->encrypt($inputPassword, [$publicKeyImported]);
 $encryptedBase64 = base64_encode($encrypted);
 
-// If you want to move away from the Pure technology without losing user passwords, insert the Recovery Key and call
-// the following function:
-// This will decrypt the encrypted password hashes. After this you can remove the PHE data and continue using user password hashes
+// Decrypt:
+$decrypted = $virgilCrypto->decrypt($encrypted, $privateKeyImported);
 
-$decrypted = $virgilCrypto->decrypt($encrypted, $privateKeyImportDER);
-
+// For dev:
 // Check status and the output of the variables
-
 printf("Recovery status: %s\n%s", $inputPassword==$decrypted ? "Success" : "Failed", "<hr>");
-var_dump($privateKeyPEM, $publicKeyPEM, $encrypted, $encryptedBase64, $decrypted);
+var_dump($encrypted, $encryptedBase64, $decrypted);
 die;
